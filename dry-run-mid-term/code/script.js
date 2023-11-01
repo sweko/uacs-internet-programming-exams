cfb881521f4bff3123684b1849673476e8f278f7
Wekoslav Stefanovski <swekster@gmail.com>
2023-11-01 20:00:12 +0100

    Added some sorting and filtering

--- dry-run-mid-term/code/script.js
+++ dry-run-mid-term/code/script.js
@@ -1,49 +1,88 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 document.addEventListener("DOMContentLoaded", siteCode);
-function siteCode() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const data = yield loadData();
-        displayAuthors(data);
+let authors = [];
+async function siteCode() {
+    const data = await loadData();
+    authors = data;
+    fillNationalities(authors);
+    displayAuthors(authors);
+    const nameSort = document.getElementById("sort-name");
+    nameSort.addEventListener("click", sortByName);
+    const idSort = document.getElementById("sort-id");
+    idSort.addEventListener("click", sortById);
+    const applyFilterButton = document.getElementById("apply-filter");
+    applyFilterButton.addEventListener("click", applyFilter);
+    const modal = document.getElementById("biblio-details");
+    modal.addEventListener("click", () => {
+        modal.classList.add("hidden");
     });
 }
-const loadData = () => __awaiter(void 0, void 0, void 0, function* () {
+const nameSorter = (first, second) => first.name.localeCompare(second.name);
+const idSorter = (first, second) => first.id - second.id;
+const sortByName = () => {
+    const sortedAuthors = authors.toSorted(nameSorter);
+    displayAuthors(sortedAuthors);
+};
+const sortById = () => {
+    const sortedAuthors = authors.toSorted(idSorter);
+    displayAuthors(sortedAuthors);
+};
+const fillNationalities = (authors) => {
+    const filter = document.getElementById("nationality-filter");
+    const nationalities = new Set();
+    for (const author of authors) {
+        nationalities.add(author.nationality);
+    }
+    for (const nationality of nationalities) {
+        const option = document.createElement("option");
+        option.value = nationality;
+        option.innerHTML = nationality;
+        filter.appendChild(option);
+    }
+};
+const applyFilter = () => {
+    const nationalityElement = document.getElementById("nationality-filter");
+    const nationality = nationalityElement.value;
+    const aliveElement = document.getElementById("alive-filter");
+    const alive = aliveElement.value;
+    let filteredAuthors = authors;
+    if (nationality !== "all") {
+        filteredAuthors = filteredAuthors.filter(author => author.nationality === nationality);
+    }
+    if (alive !== "all") {
+        filteredAuthors = filteredAuthors.filter(author => {
+            if (alive === "yes") {
+                return author.death_date === undefined;
+            }
+            return !!author.death_date;
+        });
+    }
+    displayAuthors(filteredAuthors);
+    // if (nationality === "all") {
+    //     displayAuthors(authors);
+    //     return;
+    // }
+    // const filteredAuthors = authors.filter(author => author.nationality === nationality);
+    // displayAuthors(filteredAuthors);
+};
+const loadData = async () => {
     const dataUri = "https://raw.githubusercontent.com/sweko/uacs-internet-programming-exams/main/dry-run-mid-term/data/authors.json";
-    const response = yield fetch(dataUri);
+    const response = await fetch(dataUri);
     if (!response.ok) {
         throw new Error("The data is not there");
     }
-    const data = yield response.json();
+    const data = await response.json();
     return data;
-});
+};
 const displayAuthors = (authors) => {
     const container = document.getElementById("author-container");
+    container.innerHTML = "";
     for (const author of authors) {
         const authorRow = generateAuthorRow(author);
         container.appendChild(authorRow);
     }
 };
 const generateAuthorRow = (author) => {
-    /**
-     *       <div class="author-row">
-        <div class="author-data">27</div>
-        <div class="author-data">Adrian Tchaikovsky</div>
-        <div class="author-data">1972-08-14</div>
-        <div class="author-data">Yes</div>
-        <div class="author-data">51</div>
-        <div class="author-data">British</div>
-        <div class="author-data">14 works total</div>
-        <div class="author-data">2008 - Present</div>
-      </div>
-     */
     const row = document.createElement("div");
     row.classList.add("author-row");
     // id cell
@@ -65,7 +104,7 @@ const generateAuthorRow = (author) => {
     row.appendChild(aliveCell);
     const ageCell = document.createElement("div");
     ageCell.classList.add("author-data", "author-age");
-    ageCell.innerHTML = "---";
+    ageCell.innerHTML = getAuthorAge(author).toString();
     row.appendChild(ageCell);
     const nationalityCell = document.createElement("div");
     nationalityCell.classList.add("author-data", "author-nationality");
@@ -74,11 +113,36 @@ const generateAuthorRow = (author) => {
     const biblioCell = document.createElement("div");
     biblioCell.classList.add("author-data", "author-biblio");
     biblioCell.innerHTML = `The author has ${author.bibliography.length} books`;
+    biblioCell.addEventListener("click", () => {
+        const modal = document.getElementById("biblio-details");
+        modal.classList.remove("hidden");
+        const modalHeader = document.querySelector("#biblio-details-content h2");
+        modalHeader.innerText = `Selected bibliography for ${author.name}`;
+        const bookList = document.getElementById("biblio-details-book-list");
+        bookList.innerHTML = "";
+        for (const book of author.bibliography.toSorted((first, second) => first.year - second.year)) {
+            const bookItem = document.createElement("li");
+            bookItem.innerText = `${book.name} (${book.year})`;
+            bookList.appendChild(bookItem);
+        }
+    });
     row.appendChild(biblioCell);
     const yearsActiveCell = document.createElement("div");
     yearsActiveCell.classList.add("author-data", "author-years");
     yearsActiveCell.innerHTML = "----";
     row.appendChild(yearsActiveCell);
     return row;
+};
+const getAuthorAge = (author) => {
+    // this implementation is a bit wrong, it will show wrong results sometimes
+    const birthYear = new Date(author.birth_date).getFullYear();
+    if (author.death_date) {
+        const deathYear = new Date(author.death_date).getFullYear();
+        return deathYear - birthYear;
+    }
+    else {
+        const currentYear = new Date().getFullYear();
+        return currentYear - birthYear;
+    }
 };
 //# sourceMappingURL=script.js.map
--- dry-run-mid-term/code/script.ts
+++ dry-run-mid-term/code/script.ts
@@ -13,12 +13,99 @@ interface Author {
     bibliography: Book[];
 }
 
+type AuthorSorter = (first: Author, second: Author) => number;
+
 document.addEventListener("DOMContentLoaded", siteCode)
+
+let authors: Author[] = [];
 
 async function siteCode() {
     const data = await loadData();
-    displayAuthors(data as any);
+    authors = data;
+
+    fillNationalities(authors);
+
+    displayAuthors(authors);
+
+    const nameSort = document.getElementById("sort-name")!;
+    nameSort.addEventListener("click", sortByName);
+
+    const idSort = document.getElementById("sort-id")!;
+    idSort.addEventListener("click", sortById);
+
+    const applyFilterButton = document.getElementById("apply-filter")!;
+    applyFilterButton.addEventListener("click", applyFilter)
+
+    const modal = document.getElementById("biblio-details")!;
+    modal.addEventListener("click", () => {
+        modal.classList.add("hidden");
+    });
 }
+
+const nameSorter: AuthorSorter = (first, second) => first.name.localeCompare(second.name);
+const idSorter: AuthorSorter = (first, second) => first.id - second.id;
+
+const sortByName = () => {
+    const sortedAuthors = authors.toSorted(nameSorter);
+    displayAuthors(sortedAuthors);
+}
+
+const sortById = () => {
+    const sortedAuthors = authors.toSorted(idSorter);
+    displayAuthors(sortedAuthors);
+}
+
+const fillNationalities = (authors: Author[]) => {
+    const filter = document.getElementById("nationality-filter")!;
+
+    const nationalities = new Set<string>();
+    for (const author of authors) {
+        nationalities.add(author.nationality);
+    }
+
+    for (const nationality of nationalities) {
+        const option = document.createElement("option");
+        option.value = nationality;
+        option.innerHTML = nationality;
+        filter.appendChild(option);
+    }
+}
+
+
+const applyFilter = () => {
+    const nationalityElement = document.getElementById("nationality-filter") as HTMLSelectElement;
+    const nationality = nationalityElement.value;
+
+    const aliveElement = document.getElementById("alive-filter") as HTMLSelectElement;
+    const alive = aliveElement.value;
+
+    let filteredAuthors = authors;
+    if (nationality !== "all") {
+        filteredAuthors = filteredAuthors.filter(author => author.nationality === nationality);
+    }
+    if (alive !== "all") {
+        filteredAuthors = filteredAuthors.filter(author => {
+            if (alive === "yes") {
+                return author.death_date === undefined;
+            }
+            return !!author.death_date;
+        })
+    }
+
+    displayAuthors(filteredAuthors);
+
+
+
+    // if (nationality === "all") {
+    //     displayAuthors(authors);
+    //     return;
+    // }
+
+    // const filteredAuthors = authors.filter(author => author.nationality === nationality);
+    // displayAuthors(filteredAuthors);
+}
+
+
 
 const loadData = async () => {
     const dataUri = "https://raw.githubusercontent.com/sweko/uacs-internet-programming-exams/main/dry-run-mid-term/data/authors.json";
@@ -33,8 +120,8 @@ const loadData = async () => {
 }
 
 const displayAuthors = (authors: Author[]) => {
-
     const container = document.getElementById("author-container")!;
+    container.innerHTML = "";
     for (const author of authors) {
         const authorRow = generateAuthorRow(author);
         container.appendChild(authorRow);
@@ -42,18 +129,6 @@ const displayAuthors = (authors: Author[]) => {
 }
 
 const generateAuthorRow = (author: Author) => {
-    /**
-     *       <div class="author-row">
-        <div class="author-data">27</div>
-        <div class="author-data">Adrian Tchaikovsky</div>
-        <div class="author-data">1972-08-14</div>
-        <div class="author-data">Yes</div>
-        <div class="author-data">51</div>
-        <div class="author-data">British</div>
-        <div class="author-data">14 works total</div>
-        <div class="author-data">2008 - Present</div>
-      </div>
-     */
     const row = document.createElement("div");
     row.classList.add("author-row");
 
@@ -80,7 +155,7 @@ const generateAuthorRow = (author: Author) => {
 
     const ageCell = document.createElement("div");
     ageCell.classList.add("author-data", "author-age");
-    ageCell.innerHTML = "---";
+    ageCell.innerHTML = getAuthorAge(author).toString();
     row.appendChild(ageCell);
 
     const nationalityCell = document.createElement("div");
@@ -91,6 +166,21 @@ const generateAuthorRow = (author: Author) => {
     const biblioCell = document.createElement("div");
     biblioCell.classList.add("author-data", "author-biblio");
     biblioCell.innerHTML = `The author has ${author.bibliography.length} books`;
+    biblioCell.addEventListener("click", () => {
+        const modal = document.getElementById("biblio-details")!;
+        modal.classList.remove("hidden");
+
+        const modalHeader = document.querySelector("#biblio-details-content h2")! as HTMLHeadingElement;
+        modalHeader.innerText = `Selected bibliography for ${author.name}`;
+
+        const bookList = document.getElementById("biblio-details-book-list")! as HTMLUListElement;
+        bookList.innerHTML = "";
+        for (const book of author.bibliography.toSorted((first, second) => first.year - second.year)) {
+            const bookItem = document.createElement("li");
+            bookItem.innerText = `${book.name} (${book.year})`;
+            bookList.appendChild(bookItem);
+        }
+    })
     row.appendChild(biblioCell);
 
     const yearsActiveCell = document.createElement("div");
@@ -99,6 +189,16 @@ const generateAuthorRow = (author: Author) => {
     row.appendChild(yearsActiveCell);
 
     return row;
+}
 
-
+const getAuthorAge = (author: Author) => {
+    // this implementation is a bit wrong, it will show wrong results sometimes
+    const birthYear = new Date(author.birth_date).getFullYear();
+    if (author.death_date) {
+        const deathYear = new Date(author.death_date).getFullYear();
+        return deathYear - birthYear;
+    } else {
+        const currentYear = new Date().getFullYear();
+        return currentYear - birthYear;
+    }
 }
