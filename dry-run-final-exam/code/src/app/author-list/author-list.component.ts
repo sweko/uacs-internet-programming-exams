03ae3e1bf68022b86846445a387fea4b676a67ea
Wekoslav Stefanovski <swekster@gmail.com>
2024-01-13 17:04:57 +0100

    Initial exam setup (dry-run)

---
+++ dry-run-final-exam/code/src/app/author-list/author-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-author-list',
+  templateUrl: './author-list.component.html',
+  styleUrl: './author-list.component.css'
+})
+export class AuthorListComponent {
+
+}
