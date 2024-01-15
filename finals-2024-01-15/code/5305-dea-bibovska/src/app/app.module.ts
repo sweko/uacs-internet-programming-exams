7f18818750987281b22b07ac454aa8a395b1c0b6
Student <student@uacs.edu.mk>
2024-01-15 18:41:27 +0100

    Commit

--- finals-2024-01-15/code/5305-dea-bibovska/src/app/app.module.ts
+++ finals-2024-01-15/code/5305-dea-bibovska/src/app/app.module.ts
@@ -4,24 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { StudentService } from './services/student.service';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieEditComponent } from './movie-edit/movie-edit.component';
+import { MovieCreateComponent } from './movie-create/movie-create.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     MovieListComponent,
+    FooterComponent,
+    MovieDetailsComponent,
+    MovieEditComponent,
+    MovieCreateComponent,
+    StatisticsComponent
   ],
   imports: [
+    HttpClientModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    StudentService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- finals-2024-01-15/code/5305-dea-bibovska/src/app/movie-list/movie-list.component.ts
+++ finals-2024-01-15/code/5305-dea-bibovska/src/app/movie-list/movie-list.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MovieService } from '../movie.service';
+import { Movie } from '../models/movie';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrl: './movie-list.component.css'
+  styleUrls: ['./movie-list.component.css']
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit, OnDestroy {
 
+  movies: Movie[] = [];
+  moviesSubscription$?: Subscription;
+  selectedMovie?: Movie;
+  @ViewChild('dialog') dialog?: ElementRef<HTMLDialogElement>;
+
+  constructor(private movieService: MovieService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.moviesSubscription$ = this.movieService.getMovies().subscribe({
+      next: movies => this.movies = movies,
+      error: err => console.log(err)
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.moviesSubscription$?.unsubscribe();
+  }
+
+  deleteMovie(movie: Movie) {
+    this.selectedMovie = movie;
+    this.dialog?.nativeElement.showModal();
+  }
+
+  editMovie(movie: Movie) {
+    this.router.navigate(['/movies', movie.id, "edit"]);
+  }
+
+  viewMovie(movie: Movie) {
+    this.router.navigate(['/movies', movie.id]);
+  }
+
+  addMovie() {
+    this.router.navigate(['/movies', "create"]);
+  }
+
+  confirmModal() {
+    this.movieService.deleteMovie(this.selectedMovie!.id).subscribe({
+      next: () => {
+        this.movies = this.movies.filter(m => m.id !== this.selectedMovie!.id);
+        this.selectedMovie = undefined;
+      },
+      error: err => console.log(err)
+    });
+    this.closeModal();
+  }
+
+  closeModal() {
+    this.selectedMovie = undefined;
+    this.dialog?.nativeElement.close();
+  }
 }
