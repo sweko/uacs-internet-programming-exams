58ddb639f7d0758d4b2d06935a837e82d4f56482
Gele1904 <angelnineski@gmail.com>
2024-01-15 19:20:31 +0100

    Final angel nineski

--- finals-2024-01-15/code/5029-angel-nineski/src/app/app-routing.module.ts
+++ finals-2024-01-15/code/5029-angel-nineski/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieEditComponent } from './movie-edit/movie-edit.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieCreateComponent } from './movie-create/movie-create.component';
+import { ActorDetailsComponent } from './actor-details/actor-details.component';
+import { AboutComponent } from './about/about.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 const routes: Routes = [
-  { path: 'movie-list', component: MovieListComponent },
-  { path: '', redirectTo: '/movie-list', pathMatch: 'full' },
+  { path: 'movies', component: MovieListComponent },
+  { path: 'movies/:id', component: MovieDetailsComponent },
+  { path: 'movies/:id/edit', component: MovieEditComponent },
+  { path: 'movies/create', component: MovieCreateComponent },
+  { path: 'actor/:id', component: ActorDetailsComponent },
+  { path: 'statistics', component: StatisticsComponent },
+  { path: 'about', component: AboutComponent },
+  {path: '', redirectTo: 'movies', pathMatch: 'full'}
 ];
 
 @NgModule({
@@ -12,3 +24,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
--- finals-2024-01-15/code/5029-angel-nineski/src/app/movie-list/movie-list.component.ts
+++ finals-2024-01-15/code/5029-angel-nineski/src/app/movie-list/movie-list.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../models/movie';
+import { MovieDataService } from '../services/movie-data.service';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrl: './movie-list.component.css'
+  styleUrls: ['./movie-list.component.css'],
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit {
+  movies: Movie[] = [];
+
+  constructor(private movieDataService: MovieDataService) {}
+
+  ngOnInit(): void {
+    this.loadMovies();
+  }
+
+  private loadMovies(): void {
+    this.movieDataService.getMovies().subscribe((movies) => {
+      this.movies = movies;
+    });
+  }
+
+  deleteMovie(id: number): void {
+    if (confirm('Are you sure you want to delete this movie?')) {
+      this.movieDataService.deleteMovie(id).subscribe(() => {
+        this.loadMovies();
+      });
+    }
+  }
+  countKeys(obj: object): number {
+    return Object.keys(obj).length;
+  }
 
 }
