375c9fac8536bc035cc6fb84eb417d0d82a9d6b8
Wekoslav Stefanovski <swekster@gmail.com>
2024-01-16 00:13:06 +0100

    Added student folders

---
+++ finals-2024-01-16/code/5332-milana-sokolova/src/app/band-list/band-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './band-list.component.html',
+  styleUrl: './band-list.component.css'
+})
+export class BandListComponent {
+
+}
