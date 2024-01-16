2f66fb62d60cd50d64bacb0bd6dd3eb92fd89716
Student <student@uacs.edu.mk>
2024-01-16 19:56:17 +0100

    final

---
+++ finals-2024-01-16/code/5227-andrej-arsov/src/app/band-detail/band-detail.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BandsService } from '../services/band.service';
+import { Band } from '../models/band';
+
+@Component({
+  selector: 'app-band-detail',
+  templateUrl: './band-detail.component.html',
+  styleUrls: ['./band-detail.component.css']
+})
+export class BandDetailComponent implements OnInit {
+  band: Band | undefined;
+  yearsActive: string | undefined;
+  similarBands: Band[] = [];
+
+  constructor(private route: ActivatedRoute, private bandsService: BandsService) { }
+
+  ngOnInit(): void {
+    this.getBandDetails();
+    this.getSimilarBands();
+  }
+
+  getBandDetails(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    
+    if (id) {
+      this.bandsService.getBandById(+id).subscribe((band: Band | undefined) => {
+        this.band = band;
+        this.calculateYearsActive();
+      });
+    }
+  }
+
+  getSimilarBands(): void {
+    if (this.band && this.band.genre) {
+      this.bandsService.getSimilarBands(this.band.genre).subscribe((bands: Band[]) => {
+        this.similarBands = bands;
+      });
+    }
+  }
+
+  calculateYearsActive(): void {
+    if (this.band) {
+      const startYear = this.band.formed;
+      const endYear = this.calculateEndYear();
+      
+      if (endYear === 'present') {
+        this.yearsActive = `${startYear} - present`;
+      } else {
+        this.yearsActive = `${startYear} - ${endYear}`;
+      }
+    }
+  }
+
+  calculateEndYear(): string {
+    if (this.band && this.band.albums) {
+      const latestAlbum = this.band.albums.reduce((latest, current) => {
+        return current.year > latest.year ? current : latest;
+      });
+
+      const currentYear = new Date().getFullYear();
+
+      if (latestAlbum.year >= currentYear - 2) {
+        return 'present';
+      } else {
+        return latestAlbum.year.toString();
+      }
+    }
+
+    return 'present';
+  }
+}
