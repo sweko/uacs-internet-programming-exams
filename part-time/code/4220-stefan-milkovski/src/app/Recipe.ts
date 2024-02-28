0f0a626c3926d171fa12fba25e6e083269d7af92
Bojan <bojan14.ivanovski@gmail.com>
2024-02-28 20:28:39 +0100

    All the files at once

---
+++ part-time/code/4220-stefan-milkovski/src/app/Recipe.ts
@@ -0,0 +1,15 @@
+export interface Recipe {
+    id?: number;
+    title: string;
+    cuisine: string;
+    description: string;
+    instructions: string;
+    time: number;
+    servings: number;
+}
+
+// export interface Ingredient {
+//     name: string;
+//     quantity: number;
+//     unit: string;
+// }
