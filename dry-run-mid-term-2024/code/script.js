972be55bfed8bb4f89fc62dbfa3e9d70db42d6c6
Wekoslav Stefanovski <swekster@gmail.com>
2024-11-02 13:48:44 +0100

    Added dry-run for 2024

--- dry-run-mid-term-2024/code/script.js
+++ dry-run-mid-term-2024/code/script.js
@@ -1,40 +1,20 @@
-type Book = {
-    name: string,
-    year: number,
-    type: string // predefined list of types - "Novel", "Novella", "Non-Fiction", "Collection", "Graphic Novel"
-}
-
-type Author = {
-    id: number,
-    name: string,
-    birth_date: string, // ISO 8601 date format
-    death_date?: string, // optional, ISO 8601 date format
-    nationality: string,
-    bibliography: Book[]
-}
-
-const data:Author[] = [];
-
+const data = [];
 let currentSort = "id";
-
-const displayAuthors = (authors: Author[]) => {
-    const container = document.getElementById("author-container")!;
+const displayAuthors = (authors) => {
+    const container = document.getElementById("author-container");
     container.innerHTML = "";
     for (const author of authors) {
         displayAuthor(author);
     }
-}
-
-const displayAuthor = (author: Author) => {
+};
+const displayAuthor = (author) => {
     const container = document.getElementById("author-container");
     const row = document.createElement("div");
     row.classList.add("author-row");
-    
     row.appendChild(makeAuthorDataDiv(author.id.toString()));
     row.appendChild(makeAuthorDataDiv(author.name));
     const bdate = getBirthDate(author.birth_date);
     row.appendChild(makeAuthorDataDiv(bdate));
-
     const isAlive = getIsAlive(author);
     const isAliveDiv = document.createElement("div");
     isAliveDiv.classList.add("author-data");
@@ -43,68 +23,55 @@ const displayAuthor = (author: Author) => {
     chkIsAlive.disabled = true;
     chkIsAlive.checked = isAlive;
     isAliveDiv.appendChild(chkIsAlive);
-    row.appendChild(isAliveDiv)
-
+    row.appendChild(isAliveDiv);
     row.appendChild(makeAuthorDataDiv("--"));
     row.appendChild(makeAuthorDataDiv(author.nationality));
     row.appendChild(makeAuthorDataDiv(author.bibliography.length.toString()));
     row.appendChild(makeAuthorDataDiv("--"));
-
     container?.appendChild(row);
-}
-
-const getBirthDate = (birthDateString: string) => {
+};
+const getBirthDate = (birthDateString) => {
     const birthDate = new Date(birthDateString);
     console.log(birthDate);
     const year = birthDate.getFullYear();
-    const month = birthDate.getMonth()+1;
+    const month = birthDate.getMonth() + 1;
     const day = birthDate.getDate();
     const result = `${year}-${month.toString().padStart(2, "0")}-${day.toString().padStart(2, "0")}`;
     console.log(result);
     return result;
-}
-
-const getIsAlive = (author: Author) => {
-    return author.death_date === undefined
-}
-
-const makeAuthorDataDiv = (data: string) => {
+};
+const getIsAlive = (author) => {
+    return author.death_date === undefined;
+};
+const makeAuthorDataDiv = (data) => {
     const div = document.createElement("div");
     div.classList.add("author-data");
     div.textContent = data;
     return div;
-}
-
+};
 document.addEventListener("DOMContentLoaded", async () => {
     const localData = await loadData();
     data.push(...localData);
     displayAuthors(data);
-
     const nationalities = extractNationalities(data);
     displayNationalities(nationalities);
-
-    const idSort = document.getElementById("sort-id")!;
+    const idSort = document.getElementById("sort-id");
     idSort.style.cursor = "pointer";
     idSort?.addEventListener("click", sortById);
-
-    const nameSort = document.getElementById("sort-name")!;
+    const nameSort = document.getElementById("sort-name");
     nameSort.style.cursor = "pointer";
     nameSort?.addEventListener("click", sortByName);
-
     const searchButton = document.getElementById("search-authors");
     searchButton?.addEventListener("click", searchAuthors);
-
 });
-
-const extractNationalities = (authors: Author[]) => {
+const extractNationalities = (authors) => {
     const allNationalities = authors.map(author => author.nationality);
     const unique = [...new Set(allNationalities)];
     unique.sort((f, s) => f.localeCompare(s));
     return unique;
-}
-
-const displayNationalities = (nationalities: string[]) => {
-    const select = document.getElementById("nationality-search")! as HTMLSelectElement;
+};
+const displayNationalities = (nationalities) => {
+    const select = document.getElementById("nationality-search");
     select.innerHTML = "";
     const none = document.createElement("option");
     none.text = "--- Select ---";
@@ -116,11 +83,9 @@ const displayNationalities = (nationalities: string[]) => {
         option.value = nationality;
         select.appendChild(option);
     }
-}
-
+};
 const sortById = () => {
-    const idSort = document.getElementById("sort-id")!;
-
+    const idSort = document.getElementById("sort-id");
     currentSort = "id";
     const allSorters = document.getElementsByClassName("sorter");
     for (let index = 0; index < allSorters.length; index++) {
@@ -130,16 +95,13 @@ const sortById = () => {
     }
     idSort.classList.toggle("sorted");
     idSort.classList.toggle("unsorted");
-
-    data.sort((first: Author, second: Author) => {
+    data.sort((first, second) => {
         return first.id - second.id;
     });
     displayAuthors(data);
-}
-
+};
 const sortByName = () => {
-    const nameSort = document.getElementById("sort-name")!;
-
+    const nameSort = document.getElementById("sort-name");
     currentSort = "name";
     const allSorters = document.getElementsByClassName("sorter");
     for (let index = 0; index < allSorters.length; index++) {
@@ -149,36 +111,28 @@ const sortByName = () => {
     }
     nameSort.classList.toggle("sorted");
     nameSort.classList.toggle("unsorted");
-
-    data.sort((first: Author, second: Author) => {
+    data.sort((first, second) => {
         return first.name.localeCompare(second.name);
     });
     displayAuthors(data);
-}
-
+};
 const searchAuthors = () => {
-    const nameSearch = document.getElementById("name-search")! as HTMLInputElement;
+    const nameSearch = document.getElementById("name-search");
     const nameValue = nameSearch.value.toLowerCase();
-
-    const natSearch = document.getElementById("nationality-search")! as HTMLSelectElement;
+    const natSearch = document.getElementById("nationality-search");
     const natValue = natSearch.value;
-
     const filteredAuthors = data
         .filter(author => author.name.toLowerCase().includes(nameValue))
         .filter(author => {
-            if (natValue === "") {
-                return true; // we don't filter by nationality
-            }
-            return (natValue === author.nationality);
-        });
-
-
+        if (natValue === "") {
+            return true; // we don't filter by nationality
+        }
+        return (natValue === author.nationality);
+    });
     displayAuthors(filteredAuthors);
-}
-
+};
 const loadData = async () => {
     const response = await fetch("https://raw.githubusercontent.com/sweko/uacs-internet-programming-exams/main/dry-run-mid-term-2024/data/authors.json");
     const data = await response.json();
     return data;
-}
-
+};
